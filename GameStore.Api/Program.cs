9d36269518d1f8aca05b8429884b61de0b52da2f
Muhaimin Ali <muhaiminali@ymail.com>
2024-12-20 10:45:32 -0500

    Initial commit made DTOs, Entities, Endpoints and Migrations

---
+++ GameStore.Api/Program.cs
@@ -0,0 +1,17 @@
+using GameStore.Api.Data;
+
+using GameStore.Api.Endpoints;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connString = builder.Configuration.GetConnectionString("GameStore");
+builder.Services.AddSqlite<GameStoreContext>(connString);
+
+var app = builder.Build();
+
+app.MapGamesEndpoints();
+
+
+app.Run();
+
+//ASP.NET Core Full Course For Beginners by Julio Casal
