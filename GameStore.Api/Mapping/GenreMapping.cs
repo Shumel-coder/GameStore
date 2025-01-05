5c277ddfc0b13a25382309cd1a2a447f6d37a899
Muhaimin Ali <muhaiminali@ymail.com>
2025-01-05 16:18:34 -0500

    added Genres endpoints so we cvan query genres

---
+++ GameStore.Api/Mapping/GenreMapping.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Text.RegularExpressions;
+using GameStore.Api.DTOs;
+using GameStore.Api.Entities;
+
+namespace GameStore.Api.Mapping;
+
+public static class GenreMapping
+{
+  public static GenreDTO ToDTO(this Genre genre) {
+    return new GenreDTO(genre.Id, genre.Name);
+  }
+}
